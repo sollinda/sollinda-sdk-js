bee02bbfa5b571ec90f191fd5f1c5e4e56561d54
Björn Lindfors <bjorn.lindfors@gmail.com>
2017-05-10 19:57:46 +0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let xhrRequest = require('xhr-request');
+
+const baseUrl = 'http://api.sollinda.com/v1';
+
+function request(url) {
+  return new Promise((resolve, reject) => {
+    xhrRequest(url, {json: true}, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  }).catch(e => Error('Not found'));
+}
+
+class CountryProfiles {
+  get(id, type = 'iso-alpha-2') {
+    return request(`${baseUrl}/country-profiles/${type}/${id}`);
+  }
+  list() {
+    return request(`${baseUrl}/country-profiles/list`);
+  }
+}
+
+class SollindaSDK {
+  constructor(opts) {
+    this.countryProfiles = new CountryProfiles(opts);
+  }
+}
+
+module.exports = SollindaSDK;
